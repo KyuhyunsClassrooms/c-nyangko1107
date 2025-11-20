382b2ce7a1778f3c14018b12ba74651768af2865
nyangko1107 <johngo11071107@gmail.com>
2025-11-20 02:00:07 +0000

    3 하다 맘

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -1,36 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/*
-    1. [설계]에서 정의한 변수, 배열, (필요하면) 구조체를 여기에 선언하세요.
-    
-    예시 - 배열 사용:
-    char names[100][20];
-    int scores[100];
-    int count = 0;
-    
-    예시 - 구조체 사용 (선택):
-    struct Student {
-        char name[20];
-        int score;
-    };
-    struct Student students[100];
-*/
-
-
-/*
-    2. [알고리즘]에서 설계한 핵심 기능 함수들을 여기에 선언하세요.
-*/
-
+// 기본값 ±5 범위 랜덤 생성 함수
+int rand_stat(int base) {
+    return base + (rand() % 11 - 5);
+}
 
 int main() {
-    
-    printf("--- C언어 미니 프로젝트 시작! ---\n");
+    srand(time(NULL));
 
-    /*
-        3. [알고리즘]에서 설계한 main 함수의 흐름을
-           여기에 C언어로 자유롭게 구현하세요.
-    */
-    
-    
+    // 기본 종족값 (HP, ATK, SPD)
+    int bulba_base[3] = {45, 49, 45};
+    int char_base[3]  = {39, 52, 65};
+    int squirt_base[3] = {44, 48, 43};
+int a;
+    // 실제 랜덤 능력치
+    int Bulbasaur[3];
+    int Charmander[3];
+    int Squirtle[3];
+    int* PokemonList[3] = { Bulbasaur, Charmander, Squirtle };
+
+    // 기본값에 랜덤 적용
+    for (int i = 0; i < 3; i++) {
+        Bulbasaur[i]  = rand_stat(bulba_base[i]);
+        Charmander[i] = rand_stat(char_base[i]);
+        Squirtle[i]   = rand_stat(squirt_base[i]);
+    }
+    int enemyIndex = rand() % 3;  // 0~2 중 하나
+	int* Enemy = PokemonList[enemyIndex];
+	printf("%d", PokemonList[enemyIndex]);
+    printf("포켓몬을 선택하세요(숫자를 쓰세요)\n1.이상해씨\n2.파이리\n3.꼬부기");
+    scanf("%d", a); 
+
+    // 출력
+    printf("이상해씨 : HP=%d ATK=%d SPD=%d\n", Bulbasaur[0], Bulbasaur[1], Bulbasaur[2]);
+    printf("파이리   : HP=%d ATK=%d SPD=%d\n", Charmander[0], Charmander[1], Charmander[2]);
+    printf("꼬부기   : HP=%d ATK=%d SPD=%d\n", Squirtle[0], Squirtle[1], Squirtle[2]);
+
     return 0;
 }
